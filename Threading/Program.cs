16165001a544bd8c2123017afdbd1929f4b6f982
Doram13 <1991doram@gmail.com>
2019-04-07 19:27:46 +0200

    Thread thread1 = new Thread(new ThreadStart(Counting));

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace Threading
 {
@@ -10,8 +12,27 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Thread thread1 = new Thread(new ThreadStart(Counting));
+            Thread thread2 = new Thread(new ThreadStart(Counting));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            System.Console.ReadLine();
+
+        }
+
+        static void Counting()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Current iteration: {i}. Current thread: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+
+            }
         }
     }
 }
